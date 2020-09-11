cf9e9b459c785e7af96a0c0cb3713c9c8c457f13
Aquesta <gkrasnoshlyk@gmail.com>
2020-09-11 22:42:47 +0500

    hi all 2

--- Task List/ViewController.swift
+++ Task List/ViewController.swift
@@ -14,8 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("Hello Volodya")
         print("Hello Yaroslav")
+        print("Hello Gennady")
     }
-
-
 }
 
