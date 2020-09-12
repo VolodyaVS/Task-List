63bb9681d7ff62a637d1ffb4b847f5de98f8604b
Volodya <volodyavolga@protonmail.ch>
2020-09-12 16:43:44 +0300

    test

---
+++ Task List/LaunchScreenViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LaunchScreenViewController.swift
+//  Task List
+//
+//  Created by Vladimir Stepanchikov on 12.09.2020.
+//  Copyright © 2020 Vladimir Stepanchikov. All rights reserved.
+//
+
+import UIKit
+
+class LaunchScreenViewController: UIViewController {
+
+    @IBOutlet weak var indicator: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        indicator.startAnimating()
+        
+    }
+
+}
