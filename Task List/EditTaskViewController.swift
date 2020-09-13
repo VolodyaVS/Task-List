78c562a8ee4bbe6595a0023714f3c2876efbdd74
Aquesta <gkrasnoshlyk@gmail.com>
2020-09-14 00:18:09 +0500

    repair data picker

--- Task List/EditTaskViewController.swift
+++ Task List/EditTaskViewController.swift
@@ -15,28 +15,27 @@ class EditTaskViewController: UIViewController {
     @IBOutlet var saveButtonOutlet: UIBarButtonItem!
     
     var user: User!
-    private var newTaskDate = ""
+    private var newTaskDate = "22/22/2222"
     var newTask = Task(task: "", dueDate: "", done: false, priority: 1, isFavorite: false, user: nil)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         datePickerTaskOutlet.datePickerMode = .date
+        
         
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        let dateFormater = DateFormatter()
+        dateFormater.dateFormat = "yyyy-MM-dd"
+        newTaskDate = dateFormater.string(from: datePickerTaskOutlet.date)
+       
         if segue.identifier == "saveNewTask" {
             newTask.task = taskTFOutlet.text ?? "default"
             newTask.dueDate = newTaskDate
             newTask.user = user
             newTask.isFavorite = isFavoriteSwitchOutlet.isOn
         }
-    }
-    
-    private func datePickerChangeValue(_ sender: UIDatePicker) {
-        let dateFormater = DateFormatter()
-        dateFormater.dateFormat = "yyyy-MM-dd"
-        newTaskDate = dateFormater.string(from: datePickerTaskOutlet.date)
     }
 }
