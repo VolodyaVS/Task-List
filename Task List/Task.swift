d9473387a8302c72f8fed47b1fa461d1f2cc9b38
Aquesta <gkrasnoshlyk@gmail.com>
2020-09-13 23:58:39 +0500

    add task

--- Task List/Task.swift
+++ Task List/Task.swift
@@ -11,30 +11,37 @@ import UIKit
 
 struct Task {
     
-    let task: String
-    let dueDate: String
-    let done: Bool
-    let priority: Int
+    var task: String
+    var dueDate: String
+    var done: Bool
+    var priority: Int
+    var isFavorite: Bool
+    var user: User?
 }
 
 extension Task {
     
-    static func getTaskList() -> [Task] {
+    static func getTaskList(user: User?) -> [Task] {
         
-        var tasksList: [Task] = []
+        let tasksList: [Task] = DataManager.shared.tasks
         
-        let tasks = DataManager.shared.tasks
-        let dueDates = DataManager.shared.dueDates
-        let dones = DataManager.shared.dones
-        let prioritys = DataManager.shared.priority
-        
-        for index in 0..<tasks.count {
-            let tasks = Task(task: tasks[index],
-                             dueDate: dueDates[index],
-                             done: dones[index], priority: prioritys[index])
-            tasksList.append(tasks)
-        }
+//        let tasks = DataManager.shared.task
+//        let dueDates = DataManager.shared.dueDates
+//        let dones = DataManager.shared.dones
+//        let prioritys = DataManager.shared.priority
+//
+//        for index in 0..<tasks.count {
+//            let tasks = Task(task: tasks[index],
+//                             dueDate: dueDates[index],
+//                             done: dones[index], priority: prioritys[index], isFavorite: false, user: user ?? nil)
+//            tasksList.append(tasks)
+//        }
         return tasksList
+    }
+    
+    static func addTask(user: User, task: String, duedate: String, done: Bool, priority: Int, isFavorite: Bool) {
+        let task = Task(task: task, dueDate: duedate, done: done, priority: priority, isFavorite: isFavorite, user: user)
+        DataManager.shared.tasks.append(task)
     }
     //
     //    func dataFormatter (dateString: String) -> Date {
