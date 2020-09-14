0fa06f8bab32824693f8646e38461e9157d67e9b
Fayfer Kirill <sayfer555@gmail.com>
2020-09-14 20:18:52 +0600

    added favorite VC. create swipe actions

---
+++ Task List/ViewControllers/FavoritesTasksTableViewController.swift
@@ -0,0 +1,70 @@
+//
+//  FavoritesTasksTableViewController.swift
+//  Task List
+//
+//  Created by Кирилл Файфер on 14.09.2020.
+//  Copyright © 2020 Vladimir Stepanchikov. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesTasksTableViewController: UITableViewController {
+    // MARK: - Public Properties
+    var tasks = Task.getTaskList(user: nil)
+    
+    // MARK: - Override methods
+    override func viewWillAppear(_ animated: Bool) {
+        self.tabBarController?.navigationItem.leftBarButtonItem = nil
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        var counter = 0
+        
+        for task in tasks {
+            if task.isFavorite {
+                counter += 1
+            }
+        }
+        return counter
+    }
+    
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "favoritesTasks", for: indexPath)
+        
+        if tasks[indexPath.row].isFavorite {
+            cell.textLabel?.text = tasks[indexPath.row].task
+        }
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    // MARK: - Swipe Actions
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let delete = deleteAction(at: indexPath)
+
+        return UISwipeActionsConfiguration(actions: [delete])
+    }
+
+    func deleteAction(at indexPath: IndexPath) -> UIContextualAction {
+        let action = UIContextualAction(style: .destructive, title: "Delete") { (action, view, complition) in
+            var index = indexPath.row
+
+            for favorite in 0...indexPath.row {
+                if !self.tasks[favorite].isFavorite {
+                    index += 1
+                }
+            }
+            self.tasks[index].isFavorite.toggle()
+            self.tableView.deleteRows(at: [indexPath], with: .fade)
+            
+            complition(true)
+        }
+        action.backgroundColor = .red
+
+        return action
+    }
+}
--- TaskViewController.swift
+++ TaskViewController.swift
@@ -13,21 +13,27 @@ class TaskViewController: UITableViewController {
     // MARK: - Public Properties
     var tasks = Task.getTaskList(user: nil)
     var user: User!
-    
     // MARK: - Private properties
     private var isAscending = true
     
     // MARK: - Override methods
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        sorting()
-        let sortButton = UIBarButtonItem(image: UIImage(systemName: "arrow.down"),
+    override func viewWillAppear(_ animated: Bool) {
+                let sortButton = UIBarButtonItem(image: UIImage(systemName: "arrow.down"),
                                          style: .plain, target: self,
                                          action: #selector(inversedSorting))
         
         self.tabBarController?.navigationItem.leftBarButtonItems = [sortButton]
-        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        sorting()
+//        let sortButton = UIBarButtonItem(image: UIImage(systemName: "arrow.down"),
+//                                         style: .plain, target: self,
+//                                         action: #selector(inversedSorting))
+//
+//        self.tabBarController?.navigationItem.leftBarButtonItems = [sortButton]
     }
     
     // MARK: - Table view data source
@@ -36,7 +42,6 @@ class TaskViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         let cell = tableView.dequeueReusableCell(withIdentifier: "task", for: indexPath) as! TaskViewCell
         let task = tasks[indexPath.row]
         cell.taskDescriptionLabel.text = task.task
@@ -62,6 +67,30 @@ class TaskViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    // MARK: - Swipe Actions
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let favorite = favoriteAction(at: indexPath)
+
+        return UISwipeActionsConfiguration(actions: [favorite])
+    }
+    
+    func favoriteAction(at indexPath: IndexPath) -> UIContextualAction {
+        let actionTitle: String = tasks[indexPath.row].isFavorite ? "Unfavorite" : "Favorite"
+        
+        let action = UIContextualAction(style: .destructive, title: actionTitle) { (action, view, complition) in
+            var task = self.tasks[indexPath.row]
+    
+            task.isFavorite.toggle()
+            self.tasks[indexPath.row] = task
+            
+            complition(true)
+        }
+        action.backgroundColor = UIColor(red: 17/255, green: 154/255, blue: 237/255, alpha: 1)
+
+        return action
+    }
+    
     @objc func inversedSorting() {
         isAscending.toggle()
         if isAscending {
